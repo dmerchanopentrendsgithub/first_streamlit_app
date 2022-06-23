c3e0debf90be83b90554aa937e7027dee5594ff5
David <108065367+dmerchanopentrendsgithub@users.noreply.github.com>
2022-06-23 13:13:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('Breakfat Menu')
+streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 
