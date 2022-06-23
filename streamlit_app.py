728cdda6552e8cc86585fb89955dd4396182b940
David <108065367+dmerchanopentrendsgithub@users.noreply.github.com>
2022-06-23 13:13:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfat Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Jale, Spinch % Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
